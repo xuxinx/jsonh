c1b543cb3e4bd002dcca7d74bd846bc77099ff91
xuxin <xuxinmight@gmail.com>
2019-11-22 11:11:05 +0800

    Extract grpc status message

--- errorfunc/grpcstatus/grpcstatus.go
+++ errorfunc/grpcstatus/grpcstatus.go
@@ -13,5 +13,15 @@ func ErrorFunc(e error) error {
 		return e
 	}
 
-	return cerr.New(int(st.Code()), strings.Split(e.Error(), "desc = ")[1])
+	// grpc status error format
+	// rpc error: code = Code(__code__) desc = __desc__
+	msg := e.Error()
+	{
+		msgs := strings.Split(msg, " desc = ")
+		if len(msgs) > 1 {
+			msg = msgs[1]
+		}
+	}
+
+	return cerr.New(int(st.Code()), msg)
 }
